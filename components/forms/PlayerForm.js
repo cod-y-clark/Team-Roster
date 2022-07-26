1875009409d7114dc5e334bbe4ecae0a896d74a3
cod-y-clark <cody66clark@gmail.com>
2022-07-26 18:48:07 -0500

    Updating Player Form fields

--- components/forms/PlayerForm.js
+++ components/forms/PlayerForm.js
@@ -47,7 +47,7 @@ function PlayerForm({ obj }) {
     <Form onSubmit={handleSubmit}>
       <h2 className="text-white mt-5">{obj.firebaseKey ? 'Update' : 'Create'} Player</h2>
 
-      <FloatingLabel controlId="floatingInput1" label="First Name" className="mb-3">
+      <FloatingLabel controlId="floatingInput1" label="Name" className="mb-3">
         <Form.Control
           type="text"
           placeholder="Enter Name"
@@ -58,23 +58,23 @@ function PlayerForm({ obj }) {
         />
       </FloatingLabel>
 
-      <FloatingLabel controlId="floatingInput2" label="Last Name" className="mb-3">
-        <Form.Control
-          type="text"
-          placeholder="Enter Image URL"
-          name="image"
-          value={formInput.image}
-          onChange={handleChange}
-          required
-        />
-      </FloatingLabel>
-
-      <FloatingLabel controlId="floatingInput3" label="Email" className="mb-3">
+      <FloatingLabel controlId="floatingInput3" label="Role" className="mb-3">
         <Form.Control
           type="text"
           placeholder="Enter Role"
           name="role"
           value={formInput.role}
+          onChange={handleChange}
+          required
+        />
+      </FloatingLabel>
+
+      <FloatingLabel controlId="floatingInput2" label="Image" className="mb-3">
+        <Form.Control
+          type="text"
+          placeholder="Enter Image URL"
+          name="image"
+          value={formInput.image}
           onChange={handleChange}
           required
         />
