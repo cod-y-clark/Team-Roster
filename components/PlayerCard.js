e10850d0b385bd65541117b0b3d7f486561043bb
cod-y-clark <cody66clark@gmail.com>
2022-07-26 19:38:03 -0500

    Adding Player cards

---
+++ components/PlayerCard.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Link from 'next/link';
+import { deleteSinglePlayer } from '../api/playerData';
+
+export default function PlayerCard({ playerObj, onUpdate }) {
+  const deleteThisPlayer = () => {
+    if (window.confirm(`Delete ${playerObj.name}?`)) {
+      deleteSinglePlayer(playerObj.firebaseKey).then(() => onUpdate());
+    }
+  };
+
+  return (
+    <Card style={{ width: '18rem', margin: '10px' }}>
+      <Card.Img variant="top" src={playerObj.image} alt={playerObj.name} style={{ height: '400px' }} />
+      <Card.Body>
+        <Card.Title>{playerObj.name}</Card.Title>
+        <p className="card-text bold">{playerObj.show}</p>
+        {/* DYNAMIC LINK TO VIEW THE BOOK DETAILS  */}
+        <Link href={`/player/${playerObj.firebaseKey}`} passHref>
+          <Button variant="primary" className="m-2">VIEW</Button>
+        </Link>
+        {/* DYNAMIC LINK TO EDIT THE BOOK DETAILS  */}
+        <Link href={`/player/edit/${playerObj.firebaseKey}`} passHref>
+          <Button variant="info">EDIT</Button>
+        </Link>
+        <Button variant="danger" onClick={deleteThisPlayer} className="m-2">
+          DELETE
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+PlayerCard.propTypes = {
+  playerObj: PropTypes.shape({
+    firebaseKey: PropTypes.string,
+    name: PropTypes.string,
+    image: PropTypes.string,
+    show: PropTypes.string,
+  }).isRequired,
+  onUpdate: PropTypes.func.isRequired,
+};
